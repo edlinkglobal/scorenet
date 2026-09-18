c9f94493996ae58e1b736678e52b891a95bfbf22
edlinkglobal <edlinkglobal55@gmail.com>
2026-09-18 12:01:03 +0000

    Create time_greeting_service.dart

---
+++ lib/services/time_greeting_service.dart
@@ -0,0 +1,8 @@
+class TimeGreetingService {
+  static String getGreeting() {
+    final h = DateTime.now().hour;
+    if (h < 12) return "Good morning";
+    if (h < 17) return "Good afternoon";
+    return "Good evening";
+  }
+}
